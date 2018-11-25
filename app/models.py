52d46b92df9216e908a55dda24aa0c3a295d944a
Kernael92 <kernaelapuko5@gmail.com>
2018-11-25 14:49:57 +0300

    create a blog class model and a one to many relationship between the blog and the user

--- app/models.py
+++ app/models.py
@@ -4,6 +4,19 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id'))
 
     def __repr__(self):
         return f'User {self.username}'
+
+
+
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(255))
+    blog = db.Column(db.String)
+    users = db.relationship('User',backref = 'blog',lazy = "dynamic")
+
+    def __repr__(self):
+        return f'Blog {self.blog}'
